ebe0ac2e49d4711f2cfe03a37d434ad2a143df5f
Tyler Craft <tyler@blueacorn.com>
2024-02-02 10:10:04 -0500

    initial gpt sidekick button setup

---
+++ scripts/gpt/gpt.js
@@ -0,0 +1,18 @@
+const gpt = ({ detail }) => {
+    const sk = detail.data;
+    // your custom code from button.action goes here
+
+    console.log('winner winner chicken dinner');
+};
+
+const sk = document.querySelector('helix-sidekick');
+if (sk) {
+    // sidekick already loaded
+    sk.addEventListener('custom:gpt', gpt);
+} else {
+    // wait for sidekick to be loaded
+    document.addEventListener('sidekick-ready', () => {
+        document.querySelector('helix-sidekick')
+            .addEventListener('custom:gpt', gpt);
+    }, { once: true });
+}
